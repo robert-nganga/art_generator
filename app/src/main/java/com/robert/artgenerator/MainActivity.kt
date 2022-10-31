d56bb7ecc872a080c10d514dddbbc667878ab2f5
robert-nganga <robertnganga220@gmail.com>
2022-10-31 12:47:52 +0300

    added paint functionality

--- app/src/main/java/com/robert/artgenerator/MainActivity.kt
+++ app/src/main/java/com/robert/artgenerator/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.robert.artgenerator
 
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.robert.artgenerator.PaintView.Companion.colorList
+import com.robert.artgenerator.PaintView.Companion.pathList
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var path = Path()
+        var paintBrush = Paint()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val blackButton = findViewById<ImageButton>(R.id.blackButton)
+        val clearButton = findViewById<ImageButton>(R.id.clearButton)
+
+        blackButton.setOnClickListener {
+            Toast.makeText(this, "Black Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        clearButton.setOnClickListener {
+            Toast.makeText(this, "Clear Button Clicked", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+        }
     }
 }
